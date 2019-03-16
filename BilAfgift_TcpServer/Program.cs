49cda8de41cbb420bd1117c5b67c817be8073479
Mikail Fener <mika1292@edu.easj.dk>
2019-03-16 22:49:56 +0100

    Add project files.

---
+++ BilAfgift_TcpServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BilAfgift_TcpServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
