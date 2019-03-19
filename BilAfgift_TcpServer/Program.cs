917d088c08b4573765e3393cd8cb5c7b9271ddd2
Mikail Fener <mika1292@edu.easj.dk>
2019-03-19 23:19:19 +0100

    Done

--- BilAfgift_TcpServer/Program.cs
+++ BilAfgift_TcpServer/Program.cs
@@ -1,26 +1,98 @@
 ﻿using System;
+using System.IO;
 using System.Net;
 using System.Net.Sockets;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading;
+using System.Threading.Tasks;
+using BilAfgiftLibrary;
 
 namespace BilAfgift_TcpServer
 {
-    class Program
+    public class Program
     {
+        public static TcpListener serverSocket = new TcpListener(IPAddress.Any, 6789);
+
+
         static void Main(string[] args)
         {
-            IPAddress ip = IPAddress.Parse("192.168.0.13");
-            TcpListener serverSocket = new TcpListener(ip, 6789);
+            //IPAddress ip = IPAddress.Parse("192.168.0.13");
 
             Console.WriteLine("Server Starter");
             serverSocket.Start();
 
-            TcpClient connectionSocket = serverSocket.AcceptTcpClient();
-            Console.WriteLine("Server aktiveret");
+            while (true)
+            {
+                TcpClient connectionSocket = serverSocket.AcceptTcpClient();
 
-            Console.ReadLine();
+                TcpServerService service = new TcpServerService(connectionSocket);
+                Task.Factory.StartNew(() => service.DoIt());
+            }
 
-            connectionSocket.Close();
+
+
+
             serverSocket.Stop();
         }
     }
+    //public class Program
+    //{
+    //    private static Afgift afgift = new Afgift();
+    //    private static TcpListener serverSocket = new TcpListener(IPAddress.Any, 6789);
+
+    //    public static void Main(string[] args)
+    //    {
+    //        serverSocket.Start();
+    //        Console.WriteLine("Server started");
+    //        AccepterKlienter();
+
+    //    }
+
+    //    private static void AccepterKlienter()
+    //    {
+    //        while (true)
+    //        {
+    //            TcpClient newClient = serverSocket.AcceptTcpClient();
+    //            Thread t = new Thread(new ParameterizedThreadStart(NyKlient));
+    //            t.Start(newClient);
+    //        }
+    //    }
+
+    //    private static void NyKlient(object obj)
+    //    {
+    //        TcpClient client = (TcpClient) obj;
+
+    //        Console.WriteLine("Server activated by client");
+
+    //        Stream ns = client.GetStream();
+
+    //        StreamReader sr = new StreamReader(ns);
+    //        StreamWriter sw = new StreamWriter(ns);
+    //        {
+    //            sw.AutoFlush = true;
+    //        }
+
+    //        while (true)
+    //        {
+    //            sw.WriteLine("Vil du vælge personbil eller elbil?");
+    //            string msg = sr.ReadLine();
+
+    //            if (msg.ToLower().Contains("person"))
+    //            {
+    //                sw.WriteLine("Hvad er prisen på personbilen?");
+    //                string pris = sr.ReadLine();
+    //                sw.WriteLine("Afgift: " + afgift.BilAfgift(Convert.ToInt32(pris))  + " - Tryk enter for ny beregning");
+    //                sr.ReadLine();
+    //            }
+
+    //            else if (msg.ToLower().Contains("el"))
+    //            {
+    //                sw.WriteLine("Hvad er prisen på elbilen?");
+    //                string pris = sr.ReadLine();
+    //                sw.WriteLine("Afgift: " + afgift.ElBilAfgift(Convert.ToInt32(pris)) + " - Tryk enter for ny beregning");
+    //                sr.ReadLine();
+    //            }
+    //        }
+    //    }
+    //}
 }
---
+++ BilAfgift_TcpServer/TcpServerService.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+using BilAfgiftLibrary;
+
+namespace BilAfgift_TcpServer
+{
+    public class TcpServerService
+    {
+        private static Afgift afgift = new Afgift();
+
+        private TcpClient connectionSocket;
+
+        public TcpServerService(TcpClient connectionSocket)
+        {
+            this.connectionSocket = connectionSocket;
+        }
+        internal void DoIt()
+        {
+            Console.WriteLine("Klient Joined");
+
+            Stream ns = connectionSocket.GetStream();
+            StreamReader sr = new StreamReader(ns);
+            StreamWriter sw = new StreamWriter(ns);
+            sw.AutoFlush = true;
+
+            while (true)
+            {
+                sw.WriteLine("Vil du vælge personbil eller elbil?");
+                string msg = sr.ReadLine();
+
+                if (msg.ToLower().ToUpper().Contains("person"))
+                {
+                    sw.WriteLine("Hvad er prisen på personbilen?");
+                    string pris = sr.ReadLine();
+                    sw.WriteLine("Afgift: " + afgift.BilAfgift(Convert.ToInt32(pris)) + " - Tryk enter for ny beregning");
+                    sr.ReadLine();
+                }
+                else if (msg.ToLower().ToUpper().Contains("el"))
+                {
+                    sw.WriteLine("Hvad er prisen på elbilen?");
+                    string pris = sr.ReadLine();
+                    sw.WriteLine("Afgift: " + afgift.ElBilAfgift(Convert.ToInt32(pris)) +" - Tryk enter for ny beregning");
+                    sr.ReadLine();
+                }
+            }
+            ns.Close();
+            connectionSocket.Close();
+        }
+    }
+}
